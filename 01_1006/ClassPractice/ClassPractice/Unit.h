f22a031bd264a6d30c06a4a88d8fbe04ad45a4bd
MSI\romig <romi.g20408@gmail.com>
2020-10-06 16:04:05 +0900

    リファクタリングその３ 　・Unitに直接宣言していた変数を、構造体にまとめる 　・定義をまとめておくDefinition.hを作成

--- 01_1006/ClassPractice/ClassPractice/Unit.h
+++ 01_1006/ClassPractice/ClassPractice/Unit.h
@@ -1,5 +1,8 @@
 #ifndef UNIT_H
 #define UNIT_H
+
+#include "Definition.h"
+
 class Unit
 {
 public:
@@ -7,18 +10,13 @@ public:
 	Unit();
 	// デストラクタ
 	~Unit();
+
 	//ステータス表示
 	void PrintStatus();
 
 private:
-	int  m_hp;
-	int m_str;
-	int m_mag;
-	int m_tec;
-	int m_spd;
-	int m_def;
-	int m_luc;
-	int m_mde;
+	Status m_status;
+	
 };
 
 #endif // !UNIT_H
