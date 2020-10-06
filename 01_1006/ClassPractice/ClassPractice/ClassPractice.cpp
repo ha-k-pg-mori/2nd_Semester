41a1f9294302b3002c21ca1c8f716b5ecb9225dc
MSI\romig <romi.g20408@gmail.com>
2020-10-06 14:27:25 +0900

    リファクタリングその１、 ・クラス名を変更 ・クラスの定義を別のcpp、hに移動 ・よく使うヘッダーファイルのインクルードを、Common,hに集約

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -1,5 +1,5 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
+﻿#include "Common.h"
+#include "Unit.h"
 
 // Fighterクラスを作成します
 /*
@@ -18,52 +18,52 @@
 */
 
 // Fighterクラス
-class Fighter
-{
-public:
-	// コンストラクタ
-	Fighter();
-	// デストラクタ
-	~Fighter();
-	int  m_hp;
-	int m_str;
-	int m_mag;
-	int m_tec;
-	int m_spd;
-	int m_def;
-	int m_luc;
-	int m_mde;
-};
+//class Fighter
+//{
+//public:
+//	// コンストラクタ
+//	Fighter();
+//	// デストラクタ
+//	~Fighter();
+//	int  m_hp;
+//	int m_str;
+//	int m_mag;
+//	int m_tec;
+//	int m_spd;
+//	int m_def;
+//	int m_luc;
+//	int m_mde;
+//};
+//
+//Fighter::Fighter()
+//{
+//	m_hp = 0;
+//	m_str = 0;
+//	m_mag = 0;
+//	m_tec = 0;
+//	m_spd = 0;
+//	m_def = 0;
+//	m_luc = 0;
+//	m_mde = 0;
+//}
 
-Fighter::Fighter()
-{
-	m_hp = 0;
-	m_str = 0;
-	m_mag = 0;
-	m_tec = 0;
-	m_spd = 0;
-	m_def = 0;
-	m_luc = 0;
-	m_mde = 0;
-}
-
-Fighter::~Fighter()
-{
-
-}
+//Fighter::~Fighter()
+//{
+//
+//}
 
 int main()
 {
-	Fighter fighter;
-
-	printf("m_hp = %d\n",fighter.m_hp);
+	Unit fighter;
+	/*printf("ファイター\n");*/
+	printf("m_hp  = %d\n", fighter.m_hp);
 	printf("m_str = %d\n", fighter.m_str);
 	printf("m_mag = %d\n", fighter.m_mag);
 	printf("m_tec = %d\n", fighter.m_tec);
 	printf("m_spd = %d\n", fighter.m_spd);
 	printf("m_def = %d\n", fighter.m_def);
 	printf("m_luc = %d\n", fighter.m_luc);
-	printf("m_mde = %d\n",fighter.m_mde);
+	printf("m_mde = %d\n", fighter.m_mde);
 
 	system("pause");
 	return 0;
