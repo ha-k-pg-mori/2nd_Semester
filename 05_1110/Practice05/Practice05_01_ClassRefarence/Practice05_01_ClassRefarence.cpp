5f70b023d21257b5ea2b40d81688c2323e405ed3
MSI\romig <romi.g20408@gmail.com>
2020-11-10 10:36:59 +0900

    第5回実習その１
    コンストメンバ関数を組み合わせた、クラスの参照渡し実践

---
+++ 05_1110/Practice05/Practice05_01_ClassRefarence/Practice05_01_ClassRefarence.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include"CharacterBase.h"
+
+// const参照引数を持つパラメーター表示関数
+void PrintParam(const CharacterBase& character);
+
+int main()
+{
+	CharacterBase testChar;
+	PrintParam(testChar);
+
+	return 0;
+}
+
+void PrintParam(const CharacterBase& character)
+{
+	printf("CharacterBase Parameter List\n");
+	printf("----------------------------\n");
+	printf("Hp  = %3d\n", character.GetHp());
+	printf("Mp  = %3d\n", character.GetMp());
+	printf("Str = %3d\n", character.GetStr());
+	printf("Vit = %3d\n", character.GetVit());
+	printf("Int = %3d\n", character.GetInt());
+
+}
